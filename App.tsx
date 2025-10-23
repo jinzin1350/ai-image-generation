c4970cbd642f704e918ff3a33eba32c859526fa1
jinzin1350 <engi.alireza@gmail.com>
2025-10-23 14:55:56 -0400

    Fix JSX structure: correct closing div tags in Step 4 section

--- App.tsx
+++ App.tsx
@@ -371,7 +371,7 @@ function MainApp({ user }: { user: User }) {
                   </select>
                 </div>
               </div>
-              </div>
+            </div>
             </div>
 
             {/* Action Buttons */}
